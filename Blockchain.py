2f560c470cf500e8459760e30158800dd44683f3
123-code <naranjojose256@gmail.com>
2022-12-19 17:50:27 -0500

    pow functions

--- Blockchain.py
+++ Blockchain.py
@@ -31,6 +31,41 @@ class PythonBlockchain:
         while check_proof is False:
             # sha256 function to implement pow
             hash_operation =  hashlib.sha256(str(new_proof**2 - prev_proof**2 ).encode()).hexdigest()
+            if hash_operation[:4]=='0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    def hash(self,block):
+        encoded_block = json.dumps(block,sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def ischainvalid(self,block,chain):
+        prev_block = chain[0]
+        block_index = 1
+
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['prev_hash'] == self.chain[block-1['hash']]:
+
+              return True 
+
+
+
+
+        
+
+        else:
+            #ischainvalid(block)
+            return False
+
+
+
+
+
+
+            
             
 
 
